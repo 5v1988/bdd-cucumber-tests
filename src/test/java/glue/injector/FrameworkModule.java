50f4629dd5c6bf0a99b42aa227018ae42b7e9b39
Veera N <nmani0303@gmail.com>
2023-07-22 12:17:10 +0800

    remove webdrivermanager

--- src/test/java/glue/injector/FrameworkModule.java
+++ src/test/java/glue/injector/FrameworkModule.java
@@ -6,12 +6,20 @@ import com.google.inject.Inject;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import io.cucumber.guice.ScenarioScoped;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Optional;
 import model.TestConfig;
 import model.TestContext;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.yaml.snakeyaml.Yaml;
 
 public final class FrameworkModule extends AbstractModule {
@@ -42,15 +50,21 @@ public final class FrameworkModule extends AbstractModule {
   @Provides
   @ScenarioScoped
   @Inject
-  WebDriver providesBrowserInstance(TestConfig testConfig) {
+  WebDriver providesBrowserInstance(TestConfig testConfig) throws Exception {
     String browser = Optional.ofNullable(testConfig.getBrowser()).get();
     switch (browser) {
       case "firefox":
-        return WebDriverManager.firefoxdriver().create();
+        return new FirefoxDriver(new FirefoxOptions());
       case "edge":
-        return WebDriverManager.edgedriver().create();
+        return new EdgeDriver(EdgeDriverService.createDefaultService());
+      case "remote-chrome":
+        ChromeOptions chromeOptions = new ChromeOptions();
+        return new RemoteWebDriver(new URL("http://127.0.0.1:4444"), chromeOptions);
+      case "remote-firefox":
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        return new RemoteWebDriver(new URL("http://127.0.0.1:4444"), firefoxOptions);
       default:
-        return WebDriverManager.chromedriver().create();
+        return new ChromeDriver(ChromeDriverService.createDefaultService());
     }
   }
 
